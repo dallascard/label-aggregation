b3b103649681c4f24947f3cec496e50c1ca91e04
Dallas Card <dallas.card@gmail.com>
2024-03-30 15:45:39 -0400

    fixing typo

--- models/categorical_models.py
+++ models/categorical_models.py
@@ -6,7 +6,7 @@ data {
   int<lower=3> n_levels;
   vector[n_levels] priors;
   array[n_total_responses] int<lower=1, upper=n_annotators> annotator_for_response;
-  arrary[n_total_responses] int<lower=1, upper=n_items> item_for_response;
+  array[n_total_responses] int<lower=1, upper=n_items> item_for_response;
   array[n_total_responses] int<lower=1, upper=n_levels> responses;
 }
 parameters {
